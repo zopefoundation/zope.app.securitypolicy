c1315c7088c57a92e5e8fdb0fd1349acf4b1f7d8
Michael Howitz <mh@gocept.com>
2010-09-25 09:21:51 +0000

    Preparing release 3.6.1
    git-svn-id: file:///Users/mac/vcs/main/zope.app.securitypolicy/trunk@116811 62d5b8a3-27da-0310-9561-8e5933582275

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.securitypolicy',
-    version='3.6.1dev',
+    version='3.6.1',
     author='Zope Corporation and Contributors',
     author_email='zope-dev@zope.org',
     description="ZMI-based management views for zope.securitypolicy",
@@ -64,7 +64,7 @@ setup(name='zope.app.securitypolicy',
               ]),
     install_requires=['setuptools',
                       'zope.annotation',
-                      'zope.app.authentication',
+                      'zope.app.authentication >= 3.8',
                       'zope.app.form',
                       'zope.app.security',
                       'zope.browser',
